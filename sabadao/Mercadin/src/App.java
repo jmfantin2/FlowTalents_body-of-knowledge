5874e73980e710c4ebe735815d38e6d0ac66c345
Helpii <admin@helplit.com.br>
2020-12-12 10:33:30 -0300

    Prateleira funcional woohoo

--- sabadao/Mercadin/src/App.java
+++ sabadao/Mercadin/src/App.java
@@ -5,12 +5,25 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner in = new Scanner(System.in);
+
+        Prateleira prat = new Prateleira();
+
         String opt = "";
         while (!opt.equals("X")) {
             System.out.println("(P) Cadastrar produto\n(S) Executar simulação\n(X) Encerrar");
             opt = in.next().toUpperCase();
+
             if (opt.equals("P")) {
-                System.out.println("Vc escolheu P meus prbns");
+
+                System.out.println("Informe o nome do produto:\n");
+                String nome = in.next();
+
+                System.out.println("Informe o preço do produto:\n");
+                String preco = in.next();
+
+                Produto prod = new Produto(nome, Double.valueOf(preco));
+                prat.adicionarProduto(prod);
+
             } else if (opt.equals("S")) {
                 System.out.println("Vc escolheu S meus prbns");
             }
--- sabadao/Mercadin/src/Prateleira.java
+++ sabadao/Mercadin/src/Prateleira.java
@@ -2,4 +2,19 @@ import java.util.ArrayList;
 
 public class Prateleira {
   public ArrayList<Produto> produtos = new ArrayList<>();
+
+  /**
+   * a classe já tá implementando isso sem vc ver :) public Prateleira(){ produtos
+   * = null; }
+   */
+
+  public void adicionarProduto(Produto p) {
+    produtos.add(p);
+    System.out.println("\nProduto adicionado!");
+    for (Produto el : produtos) {
+      System.out.println(el.getNome() + ": " + el.getPreco());
+    }
+    System.out.println();
+  }
+
 }
