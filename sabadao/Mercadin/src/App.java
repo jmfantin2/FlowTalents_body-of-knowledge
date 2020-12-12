f60e0ca0ffd7f75e090c15054d86079e8d5b7655
Helpii <admin@helplit.com.br>
2020-12-12 10:15:22 -0300

    Início da dinâmica de produtos :)

--- sabadao/Mercadin/src/App.java
+++ sabadao/Mercadin/src/App.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner in = new Scanner(System.in);
+        String opt = "";
+        while (!opt.equals("X")) {
+            System.out.println("(P) Cadastrar produto\n(S) Executar simulação\n(X) Encerrar");
+            opt = in.next().toUpperCase();
+            if (opt.equals("P")) {
+                System.out.println("Vc escolheu P meus prbns");
+            } else if (opt.equals("S")) {
+                System.out.println("Vc escolheu S meus prbns");
+            }
+        }
+        in.close();
     }
 }
---
+++ sabadao/Mercadin/src/Prateleira.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public class Prateleira {
+  public ArrayList<Produto> produtos = new ArrayList<>();
+}
